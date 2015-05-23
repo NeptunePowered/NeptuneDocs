92043b8e7d94dd4061b56b790ee34cdca4ea3100
Jamie Mansfield <malgm@lapis.blue>
2015-05-23 14:08:35 +0100

    Fix pull request script

--- etc/pr.sh
+++ etc/pr.sh
@@ -6,7 +6,7 @@ branch=${TRAVIS_PULL_REQUEST}
 cd build
 git init
 git remote add origin git@github.com:LexBot/NeptuneDocs.git
-if git ls-remote origin | grep -sw "$branch"
+if git ls-remote origin | grep -sw "$branch"; then
     git fetch origin $branch
     git checkout $branch
 else
